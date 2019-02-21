369686d97c717e8d2fb1e9045cf367c01758df4b
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-21 07:59:08 -0500

    Data is been populated to table, but not filtered

--- client/src/components/Departamento.js
+++ client/src/components/Departamento.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
+import DataDepto from '../data/depto.json';
 
 class Departamento extends Component {
 
@@ -12,6 +13,28 @@ class Departamento extends Component {
             <div>
                 <h3>Los Departamentos</h3>
                 <p>Códigos: </p>
+
+                <table>
+                <thead>
+                  <tr>
+                    <th>Código</th>
+                    <th>Nombre</th>
+                    <th>Código Padre</th>
+                    <th>Descripción Padre</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {DataDepto.map(function(deptoDetail, key) {
+                    return (
+                      <tr key = {key}>
+                        <td>{deptoDetail.codigo}</td>
+                        <td>{deptoDetail.nombre}</td>
+                      </tr>
+                    )
+                    })}
+                </tbody>
+                </table>
+
                 <button
                     onClick={this.onNavigateHome}
                     className="btn btn-primary"
