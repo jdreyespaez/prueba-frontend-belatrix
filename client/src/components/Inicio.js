6fe35381d611ec5fdaa2fb64ef1ddaf234f005ce
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-21 09:38:42 -0500

    Adding instructions to run the project

--- client/src/components/Inicio.js
+++ client/src/components/Inicio.js
@@ -5,6 +5,8 @@ class Inicio extends Component {
         return (
             <div>
                 <h3>Correr script</h3>
+                <p>Primero se debe correr <code>node index.js</code> para que se llene los JSON y después vendrán con la información en la tabla de cada Categoría.</p>
+                <p>Nota: Cuando se clona el repo los archivos <em>depto.json</em>, <em>prov.json</em> y <em>dist.json</em> están vacíos.</p>
             </div>
         );
     }
