225cfb928bfd5b9011e6a3a0fb75bedef2be03dc
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-20 20:59:44 -0500

    2.1 The lines of the array will provide us with the two semi-structured sets we need. 2.2 first and lineArray are going to be the nests to pick

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ const callback = (err, data) => {
     const cleaned = parsed.replace(/“|”/g, "");
     const newArray = cleaned.split("\n");
     // Step 1: Making the plain text usable
-    console.log(newArray);
+    //console.log(newArray);
 
     // Step 2: Separating the elements into arrays
     newArray.forEach(element => {
-
+        // Step 2.1: Each line of the array will the input to be processed
+        const lineArray = element.split("/");
+        lineArray.forEach((code, index) =>{
+            // 2.2: first and lineArray will be the places to extract the data
+            const first = code.split(" ");
+            console.log(first);
+            console.log(lineArray);
+        })
     });
     } catch (e) {
         console.log(e);
