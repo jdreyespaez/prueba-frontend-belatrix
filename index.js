a044865851c521f976fe54068d46bcfc2836655e
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-20 20:33:40 -0500

    Limpiando las comillas para empezar a trabajar el arreglo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 
+const depto = [];
+const prov = [];
+const dist = [];
+
 const callback = (err, data) => {
     const toString = JSON.stringify(data);
-    console.log(toString);
+    const parsed = JSON.parse(toString);
+    const cleaned = parsed.replace(/“|”/g, "");
+    console.log(cleaned);
 };
 
 fs.readFile("./file.txt", "utf8", callback);
