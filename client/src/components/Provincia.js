4f08baa86d9b5688ac6495161f3e67a836feb8f9
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-21 08:39:19 -0500

    Provincia is rendered, but not filtered

--- client/src/components/Provincia.js
+++ client/src/components/Provincia.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
+import DataProv from '../data/prov.json';
 
 class Provincia extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {array: DataProv};
+      }
 
     onNavigateHome(){
         browserHistory.push('/inicio');
@@ -10,11 +16,40 @@ class Provincia extends Component {
     render () {
         return (
             <div>
-                <h3>Las Provincias</h3>
-                <p>Códigos: </p>
+                <table class="table">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col">Código</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Código Padre</th>
+                    <th scope="col">Descripción Padre</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {DataProv.map(function(provDetail, key) {
+                    return (
+                      <tr key = {key}>
+                        <td>{provDetail.codigo}</td>
+                        <td>{provDetail.nombre}</td>
+                        <td>{provDetail.lineDadCode}</td>
+                        <td>{provDetail.lineDadName}</td>
+                      </tr>
+                    )
+                    })}
+                </tbody>
+                </table>
+
+              
                 <button
-                    onClick={this.onNavigateHome}
-                    className="btn btn-primary"
+                  onClick={this.onFilter}
+                  className="btn btn-success"
+                >
+                  Filtrar
+                </button>
+
+                <button
+                  onClick={this.onNavigateHome}
+                  className="btn btn-primary"
                 >
                     Volver
                 </button>
